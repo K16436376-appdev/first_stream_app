b6a4fa59754d609905537399afde3eb197e5a647
K16436376-appdev <144028278+K16436376-appdev@users.noreply.github.com>
2023-09-10 17:27:28 -0500

    Update streamlit_app.py
    Adding a text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ def get_fruityvice_data(this_fruit_choice):
 
 #New section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 #New section to display fruityvice api response
 import requests
