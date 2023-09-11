c7917d909a5f00bd028739c4d517db77bc082526
K16436376-appdev <144028278+K16436376-appdev@users.noreply.github.com>
2023-09-11 15:17:02 -0500

    Update streamlit_app.py
    try again.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 #New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json(0))
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response)
