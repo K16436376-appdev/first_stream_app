c88a91560e47cdda8f0df3ba90dbe1fef8b65480
K16436376-appdev <144028278+K16436376-appdev@users.noreply.github.com>
2023-09-11 15:26:28 -0500

    Update streamlit_app.py
    Going to test entry of apple

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,10 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of hhe response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
