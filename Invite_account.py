450f40fe2eb3566cb22f2a4a5aed4d9e40f6d827
K16436376-appdev <144028278+K16436376-appdev@users.noreply.github.com>
2023-09-12 18:43:04 -0500

    Update Invite_account.py
    testing

--- Invite_account.py
+++ Invite_account.py
@@ -1,2 +1,8 @@
-#New section to display fruitvice api response
-streamlit.header("Invite!") 
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+streamlit.title('You will be Cordially Invited to the Exchanges')
+streamlit.header('...as soon as you submit your Account Locator')
+streamlit.text('...using the Streamlit form LINKED HERE!')
